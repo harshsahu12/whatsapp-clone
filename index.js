5d76a1d98612b643f7de24dbdd5ae5f56da0ae61
harshsahu12 <h693836@gmail.com>
2023-11-07 16:03:46 +0530

    URL setup

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://whatsapp-clone-6530.onrender.com",
+      "https://whatsapp-clone-gamma-three.vercel.app/",
     ],
     credentials: true,
   },
